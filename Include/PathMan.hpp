1122b428896cb95f823f4e141e314b978a3cf13a
Opity69 <lucasob11@gmail.com>
2024-01-27 13:14:22 +0800

    Fixed typo in CmakeLists for include targets

--- Include/PathMan.hpp
+++ Include/PathMan.hpp
@@ -3,26 +3,15 @@
 #include <filesystem>
 #include <algorithm>
 
-class PathMan
-{
-
-
+class PathMan{
 public:
     using Path = std::filesystem::path;
     bool addSearchPath( const Path& directory );
     bool removeSearchPath( const Path& directory );
-
-
     bool makeSearchPaths();
-
     bool isFound( const Path& path ) const;
-
     [[nodiscard]] bool isIncluded( const Path& absPath ) const;
-
-
     [[nodiscard]] Path getFullPath( const Path& relativePath ) const;
-
-
     const std::vector<Path>& currentSearchPaths() const { return searchPaths; }
 
 private:
